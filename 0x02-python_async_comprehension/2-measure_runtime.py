5ba038d0b68492167df23a4ccced8d47134a7f23
JuanSebastianGB <3393@holbertonschool.com>
2022-06-08 21:42:40 -0500

    Fixing pycodestyle issue

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """
-    It creates four tasks, each of which runs an async comprehension, and then waits for all four tasks
+    It creates four tasks, each of which runs an async comprehension,
+    and then waits for all four tasks
     to complete
     :return: Total time spent in float notation.
     """
