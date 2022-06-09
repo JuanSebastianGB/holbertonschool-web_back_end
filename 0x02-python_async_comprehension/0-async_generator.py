d47a4fa809380f536d34ca2dd870102be7315e49
JuanSebastianGB <3393@holbertonschool.com>
2022-06-08 20:21:31 -0500

    Fixing implementation of generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,9 +7,11 @@ import asyncio
 from typing import Generator
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> Generator[int, float, str]:
     """
-    It's a generator that yields a random number every second
+    It's a generator that yields random numbers between 0 and 10,
+    with a 1 second delay between each
+    number
     """
     for i in range(10):
         await asyncio.sleep(1)
