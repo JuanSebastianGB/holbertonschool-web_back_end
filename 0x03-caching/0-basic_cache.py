4d583f15128d550384078dadec70fe33fe8abf93
JuanSebastianGB <3393@holbertonschool.com>
2022-06-10 20:13:20 -0500

    Refactoring the function to handle checker

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -7,7 +7,6 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     def __init__(self):
         super().__init__()
-        self.cache_data = {}
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -15,11 +14,11 @@ class BasicCache(BaseCaching):
         if key and item:
             self.cache_data[key] = item
             if len(self.cache_data) > BasicCache.MAX_ITEMS:
-                self.cache_data.popitem(last=False)
+                self.cache_data.popitem()
 
     def get(self, key):
         """ Get an item by key
         """
         if key in self.cache_data:
-            return self.cache_data.get(key)
+            return self.cache_data[key]
         return None
