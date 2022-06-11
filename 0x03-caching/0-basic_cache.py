e32488ee75e3c97d74989e16b831108f800acd66
JuanSebastianGB <3393@holbertonschool.com>
2022-06-10 20:23:03 -0500

    Fix: deleting unused constructor and pop item function

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -5,20 +5,21 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    def __init__(self):
-        super().__init__()
+    """Basic Cache that inherits from base_cache
+
+    Args:
+        BaseCaching (Class): Class to handle cache
+    """
 
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key and item:
             self.cache_data[key] = item
-            if len(self.cache_data) > BasicCache.MAX_ITEMS:
-                self.cache_data.popitem()
 
     def get(self, key):
         """ Get an item by key
         """
-        if key in self.cache_data:
+        if key in self.cache_data and self.cache_data[key]:
             return self.cache_data[key]
         return None
