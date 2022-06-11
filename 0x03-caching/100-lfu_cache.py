8846e32cb8d66eb05cc48c59d200d99775fbf73a
JuanSebastianGB <3393@holbertonschool.com>
2022-06-11 09:59:02 -0500

    Second approach

--- 0x03-caching/100-lfu_cache.py
+++ 0x03-caching/100-lfu_cache.py
@@ -14,36 +14,45 @@ class LFUCache(BaseCaching):
         self.aux_dict = {}
         super().__init__()
 
-    def _get_less_used(self):
+    def _get_less_frequency(self):
         """
-        It returns the key of the item in the dictionary with the lowest value
-        :return: The key of the least used value in the aux_dict.
+        It returns the minimum value of the dictionary.
+        :return: The minimum value in the dictionary.
         """
-        from functools import reduce
-        return reduce(lambda x, y: x if x[1] < y[1] else y,
-                      self.aux_dict.items())[0]
+        return min(self.aux_dict.values())
 
     def put(self, key, item):
         """
-        If the key and item are not empty, and the cache is not full,
-        add the key and item to the cache.
-        If the cache is full, remove the least used item from the cache
-        and the auxiliary dictionary
+        If the key and item are not empty, and the cache is full,
+        remove the least frequently used item, and add the new key and item
+        to the cache
 
         :param key: the key to be added to the cache
         :param item: the item to be added to the cache
         """
         if key and item:
             if self.cache_data.__len__() >= BaseCaching.MAX_ITEMS:
-                if self.aux_dict:
-                    less_used_key = self._get_less_used()
-                    if self.cache_data.get(less_used_key):
-                        del self.cache_data[less_used_key]
-                    if self.aux_dict.get(key):
-                        del self.aux_dict[key]
+                less_frequency = self._get_less_frequency()
+                keys_with_less_frequency = [key for key, value
+                                            in self.aux_dict.items()
+                                            if value == less_frequency]
+                if keys_with_less_frequency.__len__() > 1:
+                    keys_with_less_frequency.sort()
+                    key_to_remove = keys_with_less_frequency[0]
+                else:
+                    key_to_remove = keys_with_less_frequency[0]
 
+                self.aux_list.remove(key_to_remove)
+                del self.aux_dict[key_to_remove]
+                del self.cache_data[key_to_remove]
+
+            if self.aux_dict.get(key):
+                self.aux_dict[key] += 1
+            else:
+                self.aux_dict[key] = 1
+
+            self.aux_list.append(key)
             self.cache_data[key] = item
-            """ self.aux_list.append(item) """
 
     def get(self, key):
         """
@@ -53,10 +62,7 @@ class LFUCache(BaseCaching):
         :return: The value of the key in the cache_data dictionary.
         """
         if key in self.cache_data and key:
-            """ self.aux_list.remove(key)
-            self.aux_list.append(key) """
-            if key in self.aux_list:
-                self.aux_dict[key] += 1
-            else:
-                self.aux_dict[key] = 0
+            self.aux_dict[key] += 1
+            self.aux_list.remove(key)
+            self.aux_list.append(key)
             return self.cache_data[key]
