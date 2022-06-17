cc3f8b80bcad3d4e16dde83d001c083681633b32
JuanSebastinGB <3393@holbertonschool.com>
2022-06-16 21:21:14 -0500

    using re.sub insted of destructuring it

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 """ Main file """
 from typing import List
-from re import sub
+import re
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """ returns a redacted message """
+    """
+    It takes a list of fields, a redaction string, a message, and a separator,
+    and returns a redacted version of the message
+
+    :param fields: A list of strings that represent the fields to be redacted
+    :type fields: List[str]
+    :param redaction: The string to replace the data with
+    :type redaction: str
+    :param message: the message to be filtered
+    :type message: str
+    :param separator: The separator between fields in the message
+    :type separator: str
+    :return: A string with the fields redacted.
+    """
     for field in fields:
-        message = sub(f'{field}=.*?{separator}',
-                      f'{field}={redaction}{separator}', message)
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
