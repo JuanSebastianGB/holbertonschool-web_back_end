671e2e2956cee5e52550f8dbea8da39bcc8f3d39
JuanSebastinGB <3393@holbertonschool.com>
2022-06-16 23:41:24 -0500

    Testing

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -65,6 +65,7 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    logger.addHandler(logging.StreamHandler())
-    logger.handlers[0].setFormatter(RedactingFormatter(PII_FIELDS))
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
     return logger
