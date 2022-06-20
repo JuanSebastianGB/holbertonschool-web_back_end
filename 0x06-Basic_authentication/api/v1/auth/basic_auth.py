15125d715e2908c970432a787eb96bc7d1ac4ed4
JuanSebastinGB <3393@holbertonschool.com>
2022-06-20 13:33:08 -0500

    7. Basic - Base64 part

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,27 @@ class BasicAuth(Auth):
     def __init__(self):
         """ __init__ """
         super().__init__()
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """
+        It takes a string that looks like this:
+        `Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==`
+        and returns this:
+        `QWxhZGRpbjpvcGVuIHNlc2FtZQ==`
+
+        :param authorization_header: The value of the Authorization header
+        :type authorization_header: str
+        :return: The base64 encoded string is being returned.
+        """
+        import re
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        match_pattern = re.compile(r'^Basic (.*)$')
+        match = match_pattern.match(authorization_header)
+        if match is None:
+            return None
+        return match.group(1)
