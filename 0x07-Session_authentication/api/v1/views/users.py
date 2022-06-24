175fe1cbb501a22ff40aa3b3aa0a19575b6ddbb3
JuanSebastinGB <3393@holbertonschool.com>
2022-06-23 21:56:27 -0500

    Fix: adding abort condition

--- 0x07-Session_authentication/api/v1/views/users.py
+++ 0x07-Session_authentication/api/v1/views/users.py
@@ -30,7 +30,8 @@ def view_one_user(user_id: str = None) -> str:
     user = User.get(user_id)
     if user is None:
         abort(404)
-    print(request.current_user)
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
     if user_id == 'me' and request.current_user is not None:
         user = request.current_user
 
