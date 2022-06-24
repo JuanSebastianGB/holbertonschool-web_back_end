f76f800d77deb88cfea8fb684f56634a8d489ed3
JuanSebastinGB <3393@holbertonschool.com>
2022-06-23 22:58:15 -0500

    .

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
-""" Module of Session Auth Routes """
-
-import os
-from models.user import User
-from flask import abort, jsonify, request
+""" session auth view
+"""
 from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
+    """ Method to login a user
     """
-    It checks if the email and password are in the request, if not it
-    returns an error, if they are, it checks if the email is in the
-    database, if not it returns an error, if it is, it checks if the
-    password is valid, if not it returns an error, if it is, it creates
-    a session and returns the user's information
-    :return: A JSON object with the user information.
-    """
-
     email = request.form.get("email")
     password = request.form.get("password")
     if not email:
@@ -25,17 +18,19 @@ def login():
     if not password:
         return jsonify({"error": "password missing"}), 400
     users = User.search({"email": email})
+
     if not users:
         return jsonify(error="no user found for this email"), 404
+
     for user in users:
         if not user.is_valid_password(password):
             return jsonify({"error": "wrong password"}), 401
         else:
             from api.v1.app import auth
-            session_id = auth.create_session(user.id)
-            session_name = os.getenv("SESSION_NAME")
+            sesion_id = auth.create_session(user.id)
+            sesion_name = os.getenv("SESSION_NAME")
             user_dict = jsonify(user.to_json())
-            user_dict.set_cookie(session_name, session_id)
+            user_dict.set_cookie(sesion_name, sesion_id)
             return user_dict
 
 
@@ -43,12 +38,7 @@ def login():
                  methods=['DELETE'],
                  strict_slashes=False)
 def logout():
-    """
-    It destroys the session and returns a 200 status code if the
-    session was destroyed, otherwise it returns a 404 status code
-    :return: A JSON object with an empty dictionary and a 200 status code.
-    """
-
+    '''self descriptive'''
     destroy_session = auth.destroy_session(request)
 
     if destroy_session:
