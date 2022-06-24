143a6415cc9b8c57af0209afa284d5dcc20a73ea
JuanSebastinGB <3393@holbertonschool.com>
2022-06-23 21:21:40 -0500

    Fixing order

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -57,10 +57,9 @@ def before_request():
         return abort(401)
     if auth.current_user(request) is None:
         return abort(403)
-    request.current_user = auth.current_user(request)
-
     if auth.authorization_header(request) and auth.session_cookie(request):
         return None, abort(401)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
