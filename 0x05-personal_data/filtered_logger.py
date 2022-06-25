f975371b6a7b92588bdc9789bcbb27eb5c2c9466
JuanSebastinGB <3393@holbertonschool.com>
2022-06-25 10:38:56 -0500

    putting out filter_datum from the class

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,6 +7,27 @@ import logging
 
 PII_FIELDS = ("name", "email", "ssn", "password", "phone")
 
+def filter_datum(fields: List[str], redaction: str,
+                message: str, separator: str) -> str:
+    """
+    It takes a list of fields, a redaction string, a message,
+    and a separator, and returns a redacted version of the message
+
+    :param fields: A list of strings that represent the fields to be
+    redacted
+    :type fields: List[str]
+    :param redaction: The string to replace the data with
+    :type redaction: str
+    :param message: the message to be filtered
+    :type message: str
+    :param separator: The separator between fields in the message
+    :type separator: str
+    :return: A string with the fields redacted.
+    """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                            f'{field}={redaction}{separator}', message)
+    return message
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
@@ -29,31 +50,9 @@ class RedactingFormatter(logging.Formatter):
         :type record: logging.LogRecord
         :return: A string.
         """
-        return self.filter_datum(list(self.fields), self.REDACTION,
+        return filter_datum(list(self.fields), self.REDACTION,
                                  logging.Formatter(self.FORMAT).format(record),
                                  self.SEPARATOR)
-
-    def filter_datum(self, fields: List[str], redaction: str,
-                     message: str, separator: str) -> str:
-        """
-        It takes a list of fields, a redaction string, a message,
-        and a separator, and returns a redacted version of the message
-
-        :param fields: A list of strings that represent the fields to be
-        redacted
-        :type fields: List[str]
-        :param redaction: The string to replace the data with
-        :type redaction: str
-        :param message: the message to be filtered
-        :type message: str
-        :param separator: The separator between fields in the message
-        :type separator: str
-        :return: A string with the fields redacted.
-        """
-        for field in fields:
-            message = re.sub(f'{field}=.*?{separator}',
-                             f'{field}={redaction}{separator}', message)
-        return message
 
 
 def get_logger() -> logging.Logger:
