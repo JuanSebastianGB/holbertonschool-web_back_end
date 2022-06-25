f04162dbc50f84a31bedf7611bcaab0cb4e9ac77
JuanSebastinGB <3393@holbertonschool.com>
2022-06-25 18:29:02 -0500

    Adding docstring

--- 0x07-Session_authentication/models/user_session.py
+++ 0x07-Session_authentication/models/user_session.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+""" Session user  module"""
 
 from models.base import Base
 
 
 class UserSession(Base):
+    """ > The UserSession class is a child of the Base
+    class and it has two attributes: user_id and
+    session_id"""
+
     def __init__(self, *args: list, **kwargs: dict):
         """ Initialize a UserSession instance """
         super().__init__(*args, **kwargs)
