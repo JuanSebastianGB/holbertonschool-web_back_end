14df97ae9f0733d7de9ed1639adc97f1f676fff4
JuanSebastinGB <3393@holbertonschool.com>
2022-06-28 17:58:02 -0500

    Creating reset password end-to-end test

--- 0x08-user_authentication_service/main.py
+++ 0x08-user_authentication_service/main.py
@@ -59,6 +59,15 @@ def log_out(session_id):
     assert response.json() == {"message": "Bienvenue"}
 
 
+def reset_password_token(email):
+    """ Validating user password reset token """
+    response = requests.post(
+        f'{URL}/reset_password', {'email': email})
+    assert response.status_code == 200
+    assert response.json() == {
+        "email": email, "reset_token": response.json()['reset_token']}
+
+
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
 NEW_PASSWD = "t4rt1fl3tt3"
@@ -74,6 +83,6 @@ if __name__ == "__main__":
     profile_logged(session_id)
     profile_unlogged()
     log_out(session_id)
-    # reset_token = reset_password_token(EMAIL)
+    reset_token = reset_password_token(EMAIL)
     # update_password(EMAIL, reset_token, NEW_PASSWD)
     # log_in(EMAIL, NEW_PASSWD)
