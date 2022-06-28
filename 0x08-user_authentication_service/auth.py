fde8eea4f8411b950761d6417f43f4b63523c290
JuanSebastinGB <3393@holbertonschool.com>
2022-06-27 19:05:12 -0500

    Adding method to create create session and return the session id

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ module to hash a password """
 
+from uuid import uuid4
 import bcrypt
 from db import DB
 from user import User
@@ -50,3 +51,41 @@ class Auth:
             hashed_password = _hash_password(password)
             created_user = self._db.add_user(email, hashed_password)
             return created_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        "Check if the given email and password are valid."
+
+        :param email: str
+        :type email: str
+        :param password: str
+        :type password: str
+        :return: True if the email and password are valid, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
+
+    def _generate_uuid(self):
+        """
+        "Generate a new UUID."
+
+        :return: str
+        """
+        return str(uuid4())
+
+    def create_session(self, email: str) -> str:
+        """ Create a new session for the user with the given email."""
+
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
