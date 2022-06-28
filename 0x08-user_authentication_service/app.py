0c8932e9e44c77851e055a9c28c31e7512feee32
JuanSebastinGB <3393@holbertonschool.com>
2022-06-27 21:16:05 -0500

    Creating a endpoint to update password

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -92,7 +92,7 @@ def profile():
     return jsonify({"email": user.email})
 
 
-@app.route('reset_password', methods=['POST'], strict_slashes=False)
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
     """
     It takes an email address as input, and returns a reset token
@@ -105,8 +105,31 @@ def get_reset_password_token():
     try:
         reset_token = AUTH.get_reset_password_token(email)
         response = {
-            "email": "<user email>",
+            "email": email,
             "reset_token": reset_token
+        }
+        return jsonify(response)
+    except Exception as err:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def reset_password():
+    """
+    It takes a reset token and a new password, and updates the user's password
+    :return: A JSON object with a message
+    """
+
+    reset_token = request.form['reset_token']
+    new_password = request.form['new_password']
+    email = request.form['email']
+    if not reset_token or not new_password or not email:
+        abort(400)
+    try:
+        user = AUTH.update_password(reset_token, new_password)
+        response = {
+            "email": user.email,
+            "message": "password updated"
         }
         return jsonify(response)
     except Exception as err:
