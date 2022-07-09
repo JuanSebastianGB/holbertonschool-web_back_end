7437619fae348c8a35072a25f073cf81a1f0079d
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-07-08 20:26:19 -0500

    Adding translations

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -39,6 +39,4 @@ def index():
 if __name__ == "__main__":
     """ This is a common Python idiom to check if the file
     is being run as a script or imported as a module."""
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=5000)
