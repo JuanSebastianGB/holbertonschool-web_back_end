8cf28bd4245eace05e3eba338e518c984785890b
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-07-08 20:54:27 -0500

    Testing translations

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -13,7 +13,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """ Locale selector. """
-    return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 class Config(object):
@@ -27,18 +27,16 @@ app.config.from_object(Config)
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """
     The function `index()` returns the rendered template `index.html`
     :return: The index.html file is being returned.
     """
 
-    return render_template('0-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
     """ This is a common Python idiom to check if the file
     is being run as a script or imported as a module."""
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
