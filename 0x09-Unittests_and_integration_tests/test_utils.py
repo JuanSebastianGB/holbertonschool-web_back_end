14556aa05c76c43ada561a73563f4f63a62165bb
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-07-09 09:23:59 -0500

    Implementing parametizer to test some cases

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,9 @@
 
 """Unittest class for utils.py"""
 
-import utils
 from utils import access_nested_map
 import unittest
+from parameterized import parameterized, parameterized_class
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -23,8 +23,3 @@ class TestAccessNestedMap(unittest.TestCase):
         Assert the output is equal to the expected result.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-    @parameterized.expand([
-        ({}, ('a', )),
-        ({'a': 1}, ('a', 'b'))
-    ])
