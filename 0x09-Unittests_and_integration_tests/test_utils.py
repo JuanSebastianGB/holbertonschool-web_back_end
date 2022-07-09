7b9955520ba400025bb049e64c822ebf395df2d2
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-07-09 09:43:26 -0500

    renaming function

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -28,8 +28,9 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
-    def test_access_nested_map_key_error(self, nested_map, path):
+    def test_access_nested_map_exception(self, nested_map, path):
         """
         Assert the output is equal to the expected result.
         """
-        self.assertRaises(KeyError, access_nested_map, nested_map, path)
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
