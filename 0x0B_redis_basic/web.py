43f1c11e278ce494b67f45d5c563a6a52b448024
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-07-19 23:00:24 -0500

    5. Implementing an expiring web cache and tracker

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Web """
+
+import requests
+import redis
+import wrapper
+from Typing import Callable
+from functools import wraps
+
+
+def count_url_wrapper(method: Callable) -> Callable:
+    """ Decorator counting how many times
+    a Url is accessed """
+    @wraps(method)
+    def wrapper(url):
+        hashed_url = f'hashed_url:{url}'
+        hashed_counter = f'hashed_counter:{url}'
+
+        response_html = method(url)
+
+        store.incr(hashed_counter)
+        store.set(hashed_url, response_html)
+        store.expire(cached_key, 10)
+        return response_html
+    return wrapper
+
+
+@count_url_wrapper
+def get_page(url: str) -> str:
+    """ Get page """
+    return requests.get(url).text
