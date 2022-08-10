c1adc6bdaaec6cf738498f470260a4cc49ae68db
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-08-09 22:35:19 -0500

    Fix: Checking function to implement validations

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,11 @@
 const cleanSet = (set, starString) => {
-  if (starString === undefined || starString === '') return '';
+  if (typeof set !== 'object') return '';
+  if (typeof starString !== 'string') return '';
+  if (starString === '') return '';
+
   const temporalArray = Array.from(set);
   const filteredArray = temporalArray.filter(
-    (el) => el !== undefined && el.includes(starString),
+    (el) => el !== undefined && el.startsWith(starString),
   );
   return filteredArray.map((element) => element.replace('bon', '')).join('-');
 };
