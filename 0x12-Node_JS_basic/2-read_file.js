aeb91de128a980f303177545fca77fce5c69c612
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-08-13 18:07:47 -0500

    USing Foreach instead map to checkit out the tests

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -3,23 +3,20 @@ const fs = require('fs');
 const countStudents = (path) => {
   try {
     const response = fs.readFileSync(path, 'utf8');
-    const students = response.split('\n');
+    const students = response.split('\n').map((student) => student.split(','));
     students.shift();
     const fields = {};
-
-    students.map((student) => {
-      const sList = student.split(',');
-      if (sList[3] in fields) fields[sList[3]].push(sList[0]);
-      else fields[sList[3]] = [sList[0]];
-      return 0;
-    });
     console.log(`Number of students: ${students.length}`);
-    Object.keys(fields).map((field) => {
-      const message = `Number of students in ${field}: ${
-        fields[field].length
-      }. List: ${fields[field].join(', ')}`;
+
+    students.forEach((student) => {
+      if (student[3] in fields) fields[student[3]].push(student[0]);
+      else fields[student[3]] = [student[0]];
+    });
+
+    Object.keys(fields).forEach((field) => {
+      let message = `Number of students in ${field}: ${fields[field].length}.`;
+      message += ` List: ${fields[field].join(', ')}`;
       console.log(message);
-      return 0;
     });
   } catch (err) {
     throw new Error('Cannot load the database');
