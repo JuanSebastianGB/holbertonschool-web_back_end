ee404e81aa75ff7a388b531e6ef53b331223a99b
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-08-13 18:21:04 -0500

    Handeling empty lines at the database file end

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -3,15 +3,18 @@ const fs = require('fs');
 const countStudents = (path) => {
   try {
     const response = fs.readFileSync(path, 'utf8');
-    const students = response.split('\n').map((student) => student.split(','));
+    const students = response
+      .trim()
+      .split('\n')
+      .map((student) => student.split(','));
     students.shift();
     const fields = {};
-    console.log(`Number of students: ${students.length}`);
 
     students.forEach((student) => {
       if (student[3] in fields) fields[student[3]].push(student[0]);
       else fields[student[3]] = [student[0]];
     });
+    console.log(`Number of students: ${students.length}`);
 
     Object.keys(fields).forEach((field) => {
       let message = `Number of students in ${field}: ${fields[field].length}.`;
