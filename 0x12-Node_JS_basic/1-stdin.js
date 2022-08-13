a9d045abc158712df0415f23ad0e0a2be21bf716
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-08-13 17:44:18 -0500

    Deleting white space

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write('Welcome to Holberton School, what is your name? \n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const inputText = process.stdin.read();
